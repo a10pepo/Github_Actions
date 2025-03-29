4d157ada4bb9556b5fda96f0c63473287745e092
Pedro Nieto <pedro.nieto@onlyfy.com>
2025-03-29 10:47:23 +0100

    adding things

--- code/terraform/main.tf
+++ code/terraform/main.tf
@@ -1,41 +1,15 @@
 terraform {
   backend "s3" {
-    bucket = "pnieto-terraform-state"
+    bucket = "pnieto-ghaction-terraform-state"
     key    = "terraform/state"
     region = "eu-west-1"  # Change to your desired AWS region
   }
 }
 
-module "api_repository" {
-  source = "./api"
-  db_host = module.rds.rds_host
-  vpc_connector_arn = module.rds.vpc_connector_arn
-  rds_root_user = var.rds_root_user
-  rds_root_pass = var.rds_root_pass
-  rds_db = var.rds_db
-}
-
-module "api_messaging" {
-  source = "./messaging"
-  db_host = module.rds.rds_host
-  rds_root_user = var.rds_root_user
-  rds_root_pass = var.rds_root_pass
-  rds_db = var.rds_db  
-}
-
-module "rds" {
-  source = "./rds"
-  rds_root_user = var.rds_root_user
-  rds_root_pass = var.rds_root_pass
-  rds_db = var.rds_db
-}
-
-output "rds_host" {
-  description = "The endpoint of the RDS instance"
-  value       = module.rds.rds_host
-}
-
-output "vpc_connector_arn" {
-  description = "The ARN of the VPC connector"
-  value = module.rds.vpc_connector_arn
+resource "aws_s3_bucket" "example" {
+  bucket = "pnieto-terraform-example-bucket-${random_string.bucket_suffix.result}"
+  tags = {
+    Name        = "example-bucket"
+    Environment = "dev"
+  }
 }
